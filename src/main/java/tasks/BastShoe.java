683c7784b8fb8bec3537e0297e9bfb70c2ad135e
Andrey <kinlydog@gmail.com>
2024-04-15 22:27:57 +1000

    Updated BastShoe solution

--- src/main/java/tasks/BastShoe.java
+++ src/main/java/tasks/BastShoe.java
@@ -1,120 +1,99 @@
 package tasks;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class BastShoe {
     static String string = "";
     static ArrayList<String> list = new ArrayList<>();
     static int index = -1;
-    static boolean undo = false;
+    static boolean undo;
 
     public static String bastShoe(String command) {
-        String[] instruction = command.split(" ");
+        if (validationFailed(command)) {
+            return "";
+        }
 
+        String[] instruction = command.split(" ");
         int task = stringToInteger(instruction[0]);
 
-        if (task < 1 && task > 5) {
+        if (list.isEmpty()) {
+            clearTheList();
+        }
+
+        if (task < 1 || task > 5) {
             return string;
+        }
+
+        if ((task == 1 || task == 2) && undo) {
+            clearTheList();
         }
 
         String argument = instruction.length > 1 ? instruction[1] : "";
 
         switch (task) {
-            case 1:
-                return add(argument);
-            case 2:
-                return delete(argument);
-            case 3:
+            case 1 -> add(argument);
+            case 2 -> delete(argument);
+            case 3 -> {
                 return get(argument);
-            case 4:
-                return undo();
-            case 5:
-                return redo();
-
+            }
+            case 4 -> undo();
+            case 5 -> redo();
         }
 
         return string;
+    }
 
+    private static boolean validationFailed(String s) {
+        return s.isEmpty() || s.charAt(0) < 48 || s.charAt(0) > 57;
     }
 
     private static int stringToInteger(String s) {
         return Integer.parseInt(s);
     }
 
-    private static String add(String s) {
-        if (undo) {
-            clear();
-        }
-
+    private static void add(String s) {
         string += s;
         list.add(string);
         index++;
-
-        return string;
     }
 
-    private static String delete(String s) {
-        if (undo) {
-            clear();
-        }
-
+    private static void delete(String s) {
         int n = stringToInteger(s);
 
         if (n > string.length()) {
-            string = "";
-        } else {
-            string = string.substring(0, string.length() - n);
+            n = string.length();
         }
+
+        string = string.substring(0, string.length() - n);
 
         list.add(string);
         index++;
-
-        return string;
     }
 
     private static String get(String s) {
         int i = stringToInteger(s);
 
-        if (i >= string.length()) {
-            return "";
-        }
-
-        return String.valueOf(string.charAt(i));
+        return i >= string.length() ? "" : string.charAt(i) + "";
     }
 
-    private static String undo() {
-        if (index <= 0) {
-            return string;
+    private static void undo() {
+        if (index > 0) {
+            undo = true;
+            string = list.get(--index);
         }
-
-        undo = true;
-
-        return string = list.get(--index);
     }
 
-    private static String redo() {
-        if (index >= list.size() - 1) {
-            return string;
+    private static void redo() {
+        if (index < list.size() - 1) {
+            string = list.get(++index);
         }
-
-        return string = list.get(++index);
     }
 
-    private static void clear() {
+    private static void clearTheList() {
         undo = false;
 
         list.clear();
         index = 0;
         list.add(string);
-    }
-
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        while (true) {
-            System.out.println(bastShoe(scan.nextLine()));
-        }
     }
 }
