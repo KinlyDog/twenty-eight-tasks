b3abe97d10fe4b4e3e271d145286eab2fe8d510f
Andrey <kinlydog@gmail.com>
2024-04-28 13:35:35 +1000

    Updated MatrixTurn solution and tests (completed)

--- src/main/java/tasks/MatrixTurn.java
+++ src/main/java/tasks/MatrixTurn.java
@@ -3,13 +3,58 @@ package tasks;
 public class MatrixTurn {
     public static void matrixTurn(String[] matrix, int m, int n, int t) {
         String[] stringMatrix = new String[m / 2];
-        int u = 0;
 
+        stringMatrix = matrixToString(matrix, m, n, stringMatrix);
+        stringMatrix = lineShifter(stringMatrix, t);
+
+        char[][] chartrix = replaceMatrix(stringMatrix, m, n);
+
+        for (int i = 0; i < chartrix.length; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < chartrix[i].length; j++) {
+                sb.append(chartrix[i][j]);
+            }
+
+            matrix[i] = sb.toString();
+        }
+    }
+
+    private static char[][] replaceMatrix(String[] stringMatrix, int m, int n) {
         char[][] chartrix = new char[m][n];
 
-        for (int i = 0; i < m; i++) {
-            chartrix[i] = matrix[i].toCharArray();
+        int u = 0;
+
+        while (u < Math.min(m, n) / 2) {
+            int ind = 0;
+
+            int a = n - 1 - u;
+            int b = m - 1 - u;
+
+            for (int i = 0 + u; i < a; i++, ind++) {
+                chartrix[u][i] = stringMatrix[u].charAt(ind);
+            }
+
+            for (int i = 0 + u; i < b; i++, ind++) {
+                chartrix[i][a] = stringMatrix[u].charAt(ind);
+            }
+
+            for (int i = a; i > 0 + u; i--, ind++) {
+                chartrix[b][i] = stringMatrix[u].charAt(ind);
+            }
+
+            for (int i = b; i > 0 + u; i--, ind++) {
+                chartrix[i][u] = stringMatrix[u].charAt(ind);
+            }
+
+            u++;
         }
+
+        return chartrix;
+    }
+
+    private static String[] matrixToString(String[] matrix, int m, int n, String[] stringMatrix) {
+        int u = 0;
 
         while (u < Math.min(m, n) / 2) {
             StringBuilder sb = new StringBuilder();
@@ -21,7 +66,7 @@ public class MatrixTurn {
                 sb.append(matrix[u].charAt(i));
             }
 
-            for (int i = 1 + u; i < b; i++) {
+            for (int i = 0 + u; i < b; i++) {
                 sb.append(matrix[i].charAt(a));
             }
 
@@ -37,31 +82,23 @@ public class MatrixTurn {
             u++;
         }
 
-
-
-        stringMatrix = lineShifter(stringMatrix, t);
-
-        for (int i = 0; i < stringMatrix.length; i++) {
-            for (int j = 0; j < stringMatrix[i].length(); j++) {
-
-            }
-        }
-
-
-
-
-        System.out.println();
+        return stringMatrix;
     }
 
     private static String[] lineShifter(String[] strings, int offsetDistance) {
         String[] offsetLines = new String[strings.length];
 
         for (int i = 0; i < strings.length; i++) {
-            offsetDistance %= strings[i].length();
+            int stringLen = strings[i].length();
+
+            if (offsetDistance >= stringLen) {
+                offsetDistance %= stringLen;
+            }
+
             StringBuilder sb = new StringBuilder();
 
-            sb.append(strings[i].substring(offsetDistance));
-            sb.append(strings[i].substring(0, offsetDistance));
+            sb.append(strings[i].substring(stringLen - offsetDistance));
+            sb.append(strings[i].substring(0, stringLen - offsetDistance));
 
             offsetLines[i] = sb.toString();
         }
--- src/test/java/tasks/MatrixTurnTest.java
+++ src/test/java/tasks/MatrixTurnTest.java
@@ -10,20 +10,20 @@ class MatrixTurnTest {
     @Test
     void test0() {
         String[] defaultMatrix = {"123456", "234567", "345678", "456789"};
-        String[] turning = {"432123, 565434, 676545, 789876"};
+        String[] turning = {"432123", "565434", "676545", "789876"};
 
-        matrixTurn(defaultMatrix, 4, 6, 8);
+        matrixTurn(defaultMatrix, 4, 6, 3);
 
         assertArrayEquals(turning, defaultMatrix);
     }
 
-//    @Test
-//    void test1() {
-//        String[] defaultMatrix = {"123456", "234567", "345678", "456789"};
-//        String[] turning = {"212345, 343456, 456767, 567898"};
-//
-//        matrixTurn(defaultMatrix, 4, 6, 1);
-//
-//        assertArrayEquals(turning, defaultMatrix);
-//    }
+    @Test
+    void test1() {
+        String[] defaultMatrix = {"123456", "234567", "345678", "456789"};
+        String[] turning = {"212345", "343456", "456767", "567898"};
+
+        matrixTurn(defaultMatrix, 4, 6, 1);
+
+        assertArrayEquals(turning, defaultMatrix);
+    }
 }
